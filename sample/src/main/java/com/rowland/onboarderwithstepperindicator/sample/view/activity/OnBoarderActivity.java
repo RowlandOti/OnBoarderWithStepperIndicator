680fa51dbad41f9bc77cd2497b0b2d81de3ef494
RowlandOti <rowlandotienoo@gmail.com>
2016-06-24 02:35:27 +0300

    feat: add support for design CordinatorLayout

---
+++ sample/src/main/java/com/rowland/onboarderwithstepperindicator/sample/view/activity/OnBoarderActivity.java
@@ -0,0 +1,60 @@
+package com.rowland.onboarderwithstepperindicator.sample.view.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.rowland.onboarderwithstepperindicator.sample.R;
+import com.rowland.onboarderwithstepperindicator.view.activity.AOnBoarderWithStepperIndicatorActivity;
+import com.rowland.onboarderwithstepperindicator.view.fragment.OnBoarder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class OnBoarderActivity extends AOnBoarderWithStepperIndicatorActivity {
+
+    // Class log identifier
+    public final static String LOG_TAG = OnBoarderActivity.class.getSimpleName();
+
+    private List<OnBoarder> onBoarderPages;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStatusbarTransparent(true);
+        onBoarderPages = new ArrayList<>();
+
+        OnBoarder intro1 = new OnBoarder("Welcome", "Your Two Steps Away from Making Farming Easier", R.drawable.herdy_logo_125px);
+        intro1.setBackgroundColor(R.color.app_color_white);
+        intro1.setDescriptionColor(R.color.app_color_secondary_text_grey);
+        intro1.setTitleColor(R.color.app_color_accent_teal);
+
+        onBoarderPages.add(intro1);
+        onBoarderPages.add(intro1);
+        onBoarderPages.add(intro1);
+        onBoarderPages.add(intro1);
+        setOnboardPagesReady(onBoarderPages);
+        shouldDarkenButtonsLayout(true);
+    }
+
+    @Override
+    public void onSkipButtonPressed() {
+        super.onSkipButtonPressed();
+        loadActivity();
+    }
+
+    public static Intent getCallingIntent(Context context) {
+        return new Intent(context, OnBoarderActivity.class);
+    }
+
+    @Override
+    public void onFinishButtonPressed() {
+        loadActivity();
+    }
+
+    private void loadActivity() {
+        Toast.makeText(this, "Load Next Activity", Toast.LENGTH_SHORT).show();
+    }
+}
